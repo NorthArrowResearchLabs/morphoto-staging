96ef510febee7465a60ce51af871dd222af8fb6d
Matt Reimer <matt.reimer@gmail.com>
2019-12-03 13:33:40 -0800

    [STAGING-DEPLOY]: v.

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/morphoto-staging/precache-manifest.21ee95752e7ba481819b258cf7e62fcc.js"
+  "/morphoto-staging/precache-manifest.8d3431a15bd3f91163381d34d200c247.js"
 );
 
 self.addEventListener('message', (event) => {
